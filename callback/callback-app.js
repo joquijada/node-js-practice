98f335bc7459a337c5dc95c7f3db12eec0547095
Jose Quijada <jose.quijada@verizon.com>
2019-06-13 12:51:20 -0400

    career(docs): Latest

---
+++ callback/callback-app.js
@@ -0,0 +1,35 @@
+/*
+ * Defines a function that serves as an object constructor. It accepts two arguments:
+ * 1. "data": any old string
+ * 2. "callBackObj": Holds a function in the "on" property, which in turn
+ *                   accepts another function which will get called sometime 
+ *                   later
+ *                   a function 
+ */
+function SomeConstructorFunction(data, pConsumer) {
+    this.data = data;
+    pConsumer.on('data', function () {
+        console.log(this.data);
+    });
+}
+
+// Mock transport object
+var consumer = {
+    on: function(event, callback) {
+        setTimeout(callback, 1000);
+    }
+};
+
+// called as
+var obj = new SomeConstructorFunction('foo', consumer);
+
+
+function SomeConstructorFunctionFixed(data, pConsumer) {
+    this.data = data;
+    let self = this;
+    pConsumer.on('data', function () {
+        console.log(self.data);
+    });
+}
+
+var obj2 = new SomeConstructorFunctionFixed('foo', consumer);
